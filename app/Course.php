172f862db45e84d7605336a793fb8bcdd2708863
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-11-09 14:22:38 +0300

    add filter builder

--- app/Course.php
+++ app/Course.php
@@ -2,13 +2,14 @@
 
 namespace App;
 
+use App\Filters\Course\CourseFilters;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
 
 class Course extends Model
 {
-    public function scopeFilter(Builder $builder)
+    public function scopeFilter(Builder $builder, $request)
     {
-        return $builder;
+        return (new CourseFilters($request))->filter($builder);
     }
 }
--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -9,6 +9,6 @@ class CourseController extends Controller
 {
     public function index(Request $request)
     {
-        return  Course::filter()->get();
+        return Course::filter($request)->get();
     }
 }
