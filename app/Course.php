079e46c8f920b7aa2c0969f8469bce2399845671
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-11-09 14:06:48 +0300

    add scope

--- app/Course.php
+++ app/Course.php
@@ -3,8 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Course extends Model
 {
-    //
+    public function scopeFilter(Builder $builder)
+    {
+        return $builder;
+    }
 }
